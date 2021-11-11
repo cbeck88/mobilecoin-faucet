70efa9d616b732a7d22e3f996805fcd4629f61f1
Chris Beck <beck.ct@gmail.com>
2021-11-11 15:50:54 -0700

    Add hcaptcha integration, and a way to disable via python config

--- faucet_server.py
+++ faucet_server.py
@@ -7,17 +7,34 @@ from flask import (
     url_for,
 )
 
+import requests
+
 app = Flask(__name__)
 app.secret_key = 'very extremely secret guys'
 
 PAYMENT_AMOUNT = 0.01
+# Set this to None to disable captchas
+HCAPTCHA_SITE_KEY = "d1986f6b-0e08-4980-a6dd-00f36484f80c"
+HCAPTCHA_SECRET = "0xa43F7aA369D873B361CE50EDf536ceD114EE274b"
 
 @app.route("/", methods=['GET', 'POST'])
 def faucet():
     if request.method == 'POST':
+        if HCAPTCHA_SITE_KEY:
+            # Verify captcha
+            token = request.form['h-captcha-response']
+            params = {
+                "secret": HCAPTCHA_SECRET,
+                "response": token,
+            }
+            response = requests.post("https://hcaptcha.com/siteverify", params)
+            if not response.json()['success']:
+                flash('CAPTCHA failed')
+                return redirect(url_for("faucet"))
+
         address = request.form['address']
         # TODO: send the payment
         flash('Okay, I paid you {} MOB at {}. You happy now?'.format(PAYMENT_AMOUNT, address))
         return redirect(url_for("faucet"))
     else:
-        return render_template('faucet.html')
+        return render_template('faucet.html', hcaptcha_site_key = HCAPTCHA_SITE_KEY)
