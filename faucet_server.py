5ac33fa8f2f14f6a00d76be7d169119a636064d9
Chris Beck <beck.ct@gmail.com>
2021-11-11 16:03:32 -0700

    add site-key and remoteip to the captcha verification call
    these are optional but recommended by hcaptcha

--- faucet_server.py
+++ faucet_server.py
@@ -14,6 +14,7 @@ app.secret_key = 'very extremely secret guys'
 
 PAYMENT_AMOUNT = 0.01
 # Set this to None to disable captchas
+#HCAPTCHA_SITE_KEY = None
 HCAPTCHA_SITE_KEY = "d1986f6b-0e08-4980-a6dd-00f36484f80c"
 HCAPTCHA_SECRET = "0xa43F7aA369D873B361CE50EDf536ceD114EE274b"
 
@@ -26,10 +27,12 @@ def faucet():
             params = {
                 "secret": HCAPTCHA_SECRET,
                 "response": token,
+                "sitekey": HCAPTCHA_SITE_KEY,
+                "remoteip": request.remote_addr,
             }
             response = requests.post("https://hcaptcha.com/siteverify", params)
             if not response.json()['success']:
-                flash('CAPTCHA failed')
+                flash('You must complete the CAPTCHA to receive a payment')
                 return redirect(url_for("faucet"))
 
         address = request.form['address']
