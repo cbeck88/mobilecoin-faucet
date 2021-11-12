cb3382643346c71f6ca769827f1afb8d7e36f905
Chris Beck <beck.ct@gmail.com>
2021-11-11 17:49:46 -0700

    get working database maybe

--- faucet_server.py
+++ faucet_server.py
@@ -5,6 +5,7 @@ import math
 import mobilecoin
 from mobilecoin.client import WalletAPIError
 import requests
+import sqlite3
 
 from flask import (
     Flask,
@@ -13,8 +14,13 @@ from flask import (
     render_template,
     flash,
     url_for,
+    g,
+    current_app,
 )
 
+from db import get_db
+
+DATABASE = "faucet.db"
 FULL_SERVICE_URL = os.environ.get("FULL_SERVICE", "http://localhost:9090/wallet")
 full_service_client = mobilecoin.Client(FULL_SERVICE_URL)
 
@@ -26,6 +32,39 @@ PAYMENT_AMOUNT = 0.01
 HCAPTCHA_SITE_KEY = None
 # HCAPTCHA_SITE_KEY = "d1986f6b-0e08-4980-a6dd-00f36484f80c"
 HCAPTCHA_SECRET = "0xa43F7aA369D873B361CE50EDf536ceD114EE274b"
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            DATABASE,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@app.cli.command("init-db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+app.teardown_appcontext(close_db)
+app.cli.add_command(init_db_command)
 
 @app.route("/", methods=["GET", "POST"])
 def faucet():
